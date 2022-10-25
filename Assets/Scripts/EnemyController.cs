4c2dd02457739f79b32a6d6c0b98271eee133a12
Timboja <tim.weskamp@web.de>
2022-10-25 13:25:12 +0200

    Navmesh works with prefabs

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    //public Camera cam;
+
+    public NavMeshAgent agent;
+
+    public GameObject endzone;
+
+    void Start()
+    {
+        endzone = GameObject.FindGameObjectWithTag("Endzone");
+
+        agent.SetDestination(endzone.transform.position);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Agent goes where the mouse clicks on the screen
+
+        /*
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+        */
+
+    }
+}
